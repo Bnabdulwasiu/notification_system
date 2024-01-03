11467199121ed637d4659f77f19b1c6e4e70847a
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2024-01-03 17:47:34 +0100

    Updated admin url

--- notification_project/urls.py
+++ notification_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('supers/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', include('data.urls')),
